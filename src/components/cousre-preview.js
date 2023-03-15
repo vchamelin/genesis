7e06ea50b1819020222dd0284ed8a825a230ccb4
S.Amelin <1.slava.1.amelin.1@gmail.com>
2023-03-15 19:21:56 +0200

    feat: added some style page with courses #2

--- src/components/cousre-preview.js
+++ src/components/cousre-preview.js
@@ -1,25 +1,42 @@
-import {Image ,Button} from 'react-bootstrap';
+import {Image ,Button, Dropdown} from 'react-bootstrap';
+import StarRating from './raiting';
 
 const CoursePrev = ({course}) => {
   const { title, previewImageLink, lessonsCount, rating, description } = course;
+  const { skills } = course.meta;
+  console.log(skills)
   const urlPreviewImage = previewImageLink + '/cover.webp';
-  console.log(course)
+
   return (
-    <div className="card">
+    <div className="card shadow bg-body-tertiary rounded ">
       <Image
-        className='img-fluid'
+        className='img-fluid border-0 rounded-top'
         src={urlPreviewImage}
         alt={title}
-        style={{ minHeight: '15rem' }}
+        style={{ minHeight: '17rem', objectFit: 'cover' }}
       />
-      <div className='card-body d-flex flex-column justify-content-between'  style={{ minHeight: '15rem' }}>
+      <div className='card-body d-flex flex-column justify-content-between w-100'  style={{ minHeight: '16rem' }}>
         <h3 className="card-title fw-bold text-lg mb-0 text-center align-text-bottom">{title}</h3>
         <h5 className='p-3'>{description}</h5>
+
         <p className="text-sm mb-0 text-muted">{lessonsCount} lessons</p>
       </div>
       <div className='card-footer d-flex justify-content-between align-items-center'>
-        <p className="text-sm text-muted col mb-0">Rating: {rating}</p>
-        <Button className='col-auto text-inherit'>Read more</Button>
+        <div className='d-flex'>
+          <p className="text-sm text-muted col mb-0 p-2">Rating: </p>
+          <StarRating rating={rating}/>
+        </div>
+        <div className='dropup d-flex gap-2'>
+          <Dropdown drop='up'>
+            <Dropdown.Toggle variant="warning">Skills </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {skills.map(skill => (
+                <Dropdown.Item>{skill}</Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+          <Button className='col-auto text-inherit'>Read more</Button>
+        </div>
       </div>
     </div>
   )
